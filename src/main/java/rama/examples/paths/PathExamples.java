c3d07f84e3761c8bf7cda5d791ca98a19ee1fc01
Nathan Marz <nathan@redplanetlabs.com>
2023-12-26 12:46:50 -1000

    update path examples

--- src/main/java/rama/examples/paths/PathExamples.java
+++ src/main/java/rama/examples/paths/PathExamples.java
@@ -4,6 +4,7 @@ import java.util.*;
 import com.rpl.rama.*;
 import com.rpl.rama.ops.Ops;
 import com.rpl.rama.ops.RamaFunction1;
+import com.rpl.rama.test.TestPState;
 
 public class PathExamples {
   public static Map genData() {
@@ -238,7 +239,7 @@ public class PathExamples {
     data.put("bc", 2);
 
     Block.select(data,
-      Path.customNavBuilder((String k) -> new CustomKey(k), "a")).out("*v")
+                 Path.customNavBuilder((String k) -> new CustomKey(k), "a")).out("*v")
          .each(Ops.PRINTLN, "Val 1:", "*v")
          .select(data,
            Path.customNavBuilder(
@@ -248,6 +249,170 @@ public class PathExamples {
          .execute();
   }
 
+  public static void mapKeyExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.key("a").termVal(0))
+           .localTransform("$$p", Path.key("b").termVal(1))
+           .localSelect("$$p", Path.stay()).out("*initVal")
+           .each(Ops.PRINTLN, "Init:", "*initVal")
+           .localTransform("$$p", Path.mapKey("a").termVal("c"))
+           .localSelect("$$p", Path.stay()).out("*newVal")
+           .each(Ops.PRINTLN, "After transform:", "*newVal")
+           .execute();
+    }
+  }
+
+  public static void setVirtualValuesExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.voidSetElem().termVal("a"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After first transform:", "*v")
+           .localTransform("$$p", Path.voidSetElem().termVal("f"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After second transform:", "*v")
+           .localTransform("$$p", Path.voidSetElem().termVal("c"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After third transform:", "*v")
+           .execute();
+    }
+  }
+
+  public static void listVirtualValuesExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.afterElem().termVal("a"))
+           .localTransform("$$p", Path.afterElem().termVal("b"))
+           .localTransform("$$p", Path.afterElem().termVal("c"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After appends:", "*v")
+           .localTransform("$$p", Path.beforeElem().termVal("d"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After prepend:", "*v")
+           .localTransform("$$p", Path.beforeIndex(2).termVal("e"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After insert:", "*v")
+           .execute();
+    }
+  }
+
+  public static void subListTransformExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.key("k").afterElem().termVal("a"))
+           .localTransform("$$p", Path.key("k").afterElem().termVal("b"))
+           .localTransform("$$p", Path.key("k").afterElem().termVal("c"))
+           .localTransform("$$p", Path.key("k").afterElem().termVal("d"))
+           .localTransform("$$p", Path.key("k").afterElem().termVal("e"))
+           .localTransform("$$p", Path.key("k").afterElem().termVal("f"))
+           .localSelect("$$p", Path.key("k")).out("*v")
+           .each(Ops.PRINTLN, "Initial nested list:", "*v")
+           .localTransform("$$p",
+                           Path.key("k")
+                               .sublist(1, 5)
+                               .term((List l) -> {
+                                 List ret = new ArrayList(l);
+                                 Collections.reverse(ret);
+                                 return ret;
+                               }))
+           .localSelect("$$p", Path.key("k")).out("*v")
+           .each(Ops.PRINTLN, "After transform:", "*v")
+           .execute();
+    }
+  }
+
+  public static void filteredListExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.afterElem().termVal(0))
+           .localTransform("$$p", Path.afterElem().termVal(1))
+           .localTransform("$$p", Path.afterElem().termVal(2))
+           .localTransform("$$p", Path.afterElem().termVal(3))
+           .localTransform("$$p", Path.afterElem().termVal(4))
+           .localTransform("$$p", Path.afterElem().termVal(5))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "Initial list:", "*v")
+           .localTransform("$$p",
+                           Path.filteredList(Path.filterPred(Ops.IS_EVEN))
+                               .term((List l) -> {
+                                 List ret = new ArrayList(l);
+                                 Collections.reverse(ret);
+                                 return ret;
+                               }))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After transform:", "*v")
+           .execute();
+    }
+  }
+
+  public static void subselectTransformExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.afterElem().termVal(null))
+           .localTransform("$$p", Path.last().afterElem().termVal(1))
+           .localTransform("$$p", Path.last().afterElem().termVal(2))
+           .localTransform("$$p", Path.last().afterElem().termVal(3))
+           .localTransform("$$p", Path.last().afterElem().termVal(4))
+           .localTransform("$$p", Path.afterElem().termVal(null))
+           .localTransform("$$p", Path.last().afterElem().termVal(5))
+           .localTransform("$$p", Path.last().afterElem().termVal(6))
+           .localTransform("$$p", Path.last().afterElem().termVal(7))
+           .localTransform("$$p", Path.afterElem().termVal(null))
+           .localTransform("$$p", Path.last().afterElem().termVal(8))
+           .localTransform("$$p", Path.last().afterElem().termVal(9))
+           .localTransform("$$p", Path.last().afterElem().termVal(10))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "Initial contents:", "*v")
+           .localTransform("$$p",
+                           Path.subselect(Path.all().all().filterPred(Ops.IS_ODD))
+                               .term((List l) -> {
+                                 List ret = new ArrayList(l);
+                                 Collections.reverse(ret);
+                                 return ret;
+                               }))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After transform:", "*v")
+           .execute();
+    }
+  }
+
+  public static void multiArityTermExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.key("a").termVal(1))
+           .localTransform("$$p", Path.key("b").termVal(2))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "Initial value:", "*v")
+           .localTransform("$$p", Path.key("a").term(Ops.PLUS, 10))
+           .localTransform("$$p",
+                           Path.key("b")
+                               .term((Integer v, Integer arg1, Integer arg2) -> {
+                                 System.out.println("term function args: " + v + ", " + arg1 + ", " + arg2);
+                                 return v * arg1 + arg2;
+                               }, 10, 20))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After transforms:", "*v")
+           .execute();
+    }
+  }
+
+  public static void termVoidExample() throws Exception {
+    try(TestPState tp = TestPState.create(Object.class)) {
+      Block.each(Ops.IDENTITY, tp).out("$$p")
+           .localTransform("$$p", Path.key("a").afterElem().termVal(1))
+           .localTransform("$$p", Path.key("a").afterElem().termVal(2))
+           .localTransform("$$p", Path.key("a").afterElem().termVal(3))
+           .localTransform("$$p", Path.key("b").termVal("xyz"))
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "Initial value:", "*v")
+           .localTransform("$$p", Path.key("a").nth(1).termVoid())
+           .localTransform("$$p", Path.key("b").termVoid())
+           .localSelect("$$p", Path.stay()).out("*v")
+           .each(Ops.PRINTLN, "After transforms:", "*v")
+           .execute();
+    }
+  }
 
   public static void main(String [] args) {
     customNavBuilderExamples();
