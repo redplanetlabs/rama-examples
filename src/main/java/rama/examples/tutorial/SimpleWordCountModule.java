99026ed4b77560e7c573b6fc073c0d349788a85c
Nathan Marz <nathan@redplanetlabs.com>
2024-08-24 04:13:01 -1000

    fix depot naming inconsistency between SimpleWordCountModule and docs

--- src/main/java/rama/examples/tutorial/SimpleWordCountModule.java
+++ src/main/java/rama/examples/tutorial/SimpleWordCountModule.java
@@ -7,12 +7,12 @@ import com.rpl.rama.test.*;
 public class SimpleWordCountModule implements RamaModule {
     @Override
     public void define(Setup setup, Topologies topologies) {
-        setup.declareDepot("*depot", Depot.random());
+        setup.declareDepot("*wordDepot", Depot.random());
 
         StreamTopology s = topologies.stream("wordCountStream");
         s.pstate("$$wordCounts", PState.mapSchema(String.class, Long.class));
 
-        s.source("*depot").out("*token")
+        s.source("*wordDepot").out("*token")
          .hashPartition("*token")
          .compoundAgg("$$wordCounts", CompoundAgg.map("*token", Agg.count()));
     }
@@ -21,7 +21,7 @@ public class SimpleWordCountModule implements RamaModule {
         try (InProcessCluster cluster = InProcessCluster.create()) {
             cluster.launchModule(new SimpleWordCountModule(), new LaunchConfig(1, 1));
             String moduleName = SimpleWordCountModule.class.getName();
-            Depot depot = cluster.clusterDepot(moduleName, "*depot");
+            Depot depot = cluster.clusterDepot(moduleName, "*wordDepot");
             depot.append("one");
             depot.append("two");
             depot.append("two");
